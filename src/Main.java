9d9203fa9334322ae0a7304d70d883901f4a69e5
Graceli95 <graceli9095@gmail.com>
2024-11-26 13:59:30 -0700

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Employee  obj0=new Employee("Ayodeji","Ade","swe",3.87);
+        Employee obj1 = new Employee("Hemalatha", "Chandrasekar", "software developer",8000.00);
+        Employee obj2 = new Employee("Jyothi", "Devarapalli", "software developer",6000.00);
+        Employee obj3 = new Employee("DeJean", "Williams", "software developer",5000.00);
+        Employee obj4 = new Employee("Grace", "li", "software developer",5000.00);
+        Employee obj5 = new Employee("Rana", "i ", "software developer",5000.00);
+
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+        System.out.println(obj4);
+        System.out.println(obj5);
+        System.out.println(obj0);
+        System.out.println(obj1.equals(obj2));
+        System.out.println(obj1.equals(obj3));
+        System.out.println(obj1.hashCode());
+
+        System.out.println(obj1.getFirstName());
     }
 }
