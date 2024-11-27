d7de3cfee6cd4f2a3022a03fdd2bce1bf8c622b6
Graceli95 <graceli9095@gmail.com>
2024-11-27 16:04:11 -0500

    add files

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,18 @@
+import java.util.Objects;
+
+/**
+ * Step 1: Set Up the Employee Class
+ */
 public class Employee {
     private String firstName;
     private String lastName;
     private String title;
     private double payRate;
 
-    public Employee() {
 
-    }
-
+    /**
+     *Step 2: Create a Parameterized Constructor
+     */
     public Employee(String firstName, String lastName, String title, double payRate) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,6 +20,9 @@ public class Employee {
         this.payRate = payRate;
     }
 
+    /**
+     * Getters and setters allow controlled access to private fields, ensuring encapsulation.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -47,24 +55,59 @@ public class Employee {
         return payRate;
     }
 
-    public double weeklySalary() {
+    public double caculateWeeklySalary() {
         return payRate * 40;
 
     }
 
+    /**
+     * Override toString Method
+     *The toString method is used to display the object’s data in a readable format.
+     */
     @Override
     public String toString() {
-        return "firstName=" + firstName + " lastName=" + lastName + ", title=" + title + "payRate=" + payRate;
+        return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", title=" + title + ", payRate=" + payRate + "]";
     }
+
+
+/**
+ * Override equals Method
+ *  check if two obj is the same obj
+ *  Override the equals method to compare two employees based on their firstName, lastName, and title.
+ * The equals method ensures logical equality, allowing you to check if two employees have the same attributes.
+ */
+
+//boolean result = obj7.equals(obj5);
+
+//    @Override
+//
+//    public boolean equals(Object obj) {
+////        to ensure obj is an employee
+//        if (obj == null || getClass() != obj.getClass())
+//            return false;
+////        cast object to employee
+//        Employee employee = (Employee) obj;
+//        return firstName.equals(employee.firstName)
+//                && lastName.equals(((Employee) obj).lastName)
+//                && title.equals(((Employee) obj).title);
+//
+//    }
+
+//boolean result = obj7.equals(obj5);
 
     @Override
-    public boolean equals(Object obj) {
-        return firstName.equals(((Employee) obj).firstName)
-                && lastName.equals(((Employee) obj).lastName)
-                && title.equals(((Employee) obj).title);
-
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(this.payRate, employee.payRate) == 0 && Objects.equals(this.firstName, employee.firstName) && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.title, employee.title);
     }
 
+    /**
+     * Override hashCode Method
+     * Task:
+     * Override the hashCode method to generate a hash code based on the same fields used in equals.
+     * The hashCode method ensures that logically equal objects have the same hash code, which is important for collections like HashSet and HashMap.
+     */
     @Override
     public int hashCode() {
         return firstName.hashCode() + lastName.hashCode() + title.hashCode();
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        Employee  obj0=new Employee("Ayodeji","Ade","swe",3.87);
+
         Employee obj1 = new Employee("Hemalatha", "Chandrasekar", "software developer",8000.00);
         Employee obj2 = new Employee("Jyothi", "Devarapalli", "software developer",6000.00);
         Employee obj3 = new Employee("DeJean", "Williams", "software developer",5000.00);
-        Employee obj4 = new Employee("Grace", "li", "software developer",5000.00);
+//        Employee obj4 = new Employee("Grace", "li", "software developer",5000.00);
+        Employee obj7 = new Employee("Rana", "i ", "software developer",5000.00);
+
         Employee obj5 = new Employee("Rana", "i ", "software developer",5000.00);
+        Employee obj6 =new Employee("Ayodeji","Ade","software developer",5000.00);
+
+        boolean result = obj7.equals(obj5);
+
+        System.out.println(result);
+
+
 
         System.out.println(obj1);
         System.out.println(obj2);
         System.out.println(obj3);
-        System.out.println(obj4);
+        System.out.println(obj7);
         System.out.println(obj5);
-        System.out.println(obj0);
+        System.out.println(obj6);
+
         System.out.println(obj1.equals(obj2));
-        System.out.println(obj1.equals(obj3));
+
+        System.out.println("the original first name " + obj3.getFirstName());
+        obj3.setFirstName(obj1.getFirstName());//obj3 is Dejean, obj1 is hema
+        System.out.println("new first name " + obj3.getFirstName());
+        obj3.setLastName(obj1.getLastName());
+        obj3.setTitle(obj1.getTitle());
+        System.out.println("comparing obj1 to obj3 " + obj1.equals(obj3));
+
         System.out.println(obj1.hashCode());
 
         System.out.println(obj1.getFirstName());
